9914f45e48809614fb6d0a8eac5448fdb06f8d43
SantanaJodi <targetsan21@gmail.com>
2019-11-13 14:21:41 +0700

    canvas App

--- 20_canvas/app.js
+++ 20_canvas/app.js
@@ -3,4 +3,14 @@ console.log('yay')
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.fillRect(20,20,250,175);
+context.fillStyle = 'orange';
+context.strokeStyle = 'red';
+
+// fillRect(x, y, width, height)
+// context.fillRect(20,20,250,175);
+
+context.fillRect(150, 75, canvas.width, canvas.height);
+context.beginPath();
+context.moveTo(100,70);
+context.lineTo(150,100);
+context.stroke();
